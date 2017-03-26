50222f7f16d9905e1ff5a137d337df181d28a101
astridx <info@astrid-guenther.de>
2017-03-26 11:49:26 +0200

    coding standard fix

--- tests/suites/adapters/ContaoAdapterTest.php
+++ tests/suites/adapters/ContaoAdapterTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    CMSScanner
  * @copyright  Copyright (C) 2016 CMS-Garden.org
@@ -21,105 +22,108 @@ use Symfony\Component\Finder\Finder;
  */
 class ContaoAdapterTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var ContaoAdapter */
-    public $object;
+	/** @var ContaoAdapter */
+	public $object;
 
-    public function setUp()
-    {
-        $this->object = new ContaoAdapter();
-    }
+	public function setUp()
+	{
+		$this->object = new ContaoAdapter();
+	}
 
-    public function testCorrectNameIsReturned()
-    {
-        $this->assertEquals('Contao', $this->object->getName());
-    }
+	public function testCorrectNameIsReturned()
+	{
+		$this->assertEquals('Contao', $this->object->getName());
+	}
 
-    public function testSystemsAreDetected()
-    {
-        $finder = Finder::create()
-            ->files()
-            ->in(CMSSCANNER_MOCKFILES_PATH)
-            ->name('dummy.php')
-            ->name('configuration.php')
-        ;
+	public function testSystemsAreDetected()
+	{
+		$finder = Finder::create()
+				->files()
+				->in(CMSSCANNER_MOCKFILES_PATH)
+				->name('dummy.php')
+				->name('configuration.php')
+		;
 
-        $finder = $this->object->appendDetectionCriteria($finder);
+		$finder = $this->object->appendDetectionCriteria($finder);
 
-        $results = array();
-        $falseCount = 0;
+		$results = array();
+		$falseCount = 0;
 
-        foreach ($finder as $file) {
-            $system = $this->object->detectSystem($file);
+		foreach ($finder as $file)
+		{
+			$system = $this->object->detectSystem($file);
 
-            if ($system == false) {
-                $falseCount++;
-                continue;
-            }
+			if ($system == false)
+			{
+				$falseCount++;
+				continue;
+			}
 
-            $system->version = $this->object->detectVersion($system->getPath());
+			$system->version = $this->object->detectVersion($system->getPath());
 
-            // Append successful result to array
-            $results[$system->version] = $system;
-        }
+			// Append successful result to array
+			$results[$system->version] = $system;
+		}
 
-        $this->assertCount(3, $results);
-        $this->assertEquals(17, $falseCount);
-        $this->assertArrayHasKey('2.10.4', $results);
-        $this->assertArrayHasKey('3.1.0', $results);
-        $this->assertArrayHasKey('4.1.1', $results);
-        $this->assertInstanceOf('Cmsgarden\Cmsscanner\Detector\System', current($results));
-    }
+		$this->assertCount(3, $results);
+		$this->assertEquals(17, $falseCount);
+		$this->assertArrayHasKey('2.10.4', $results);
+		$this->assertArrayHasKey('3.1.0', $results);
+		$this->assertArrayHasKey('4.1.1', $results);
+		$this->assertInstanceOf('Cmsgarden\Cmsscanner\Detector\System', current($results));
+	}
 
-    /**
-     * Tests that Contao 2 modules are correctly detected
-     */
-    public function testContao2Modules()
-    {
-        $modules = $this->object->detectModules(
-            new \SplFileInfo(CMSSCANNER_MOCKFILES_PATH . '/contao/contao2')
-        );
+	/**
+	 * Tests that Contao 2 modules are correctly detected
+	 */
+	public function testContao2Modules()
+	{
+		$modules = $this->object->detectModules(
+				new \SplFileInfo(CMSSCANNER_MOCKFILES_PATH . '/contao/contao2')
+		);
 
-        $this->assertCount(1, $modules);
-        $this->assertInstanceOf('Cmsgarden\Cmsscanner\Detector\Module', $modules[0]);
-        $this->assertEquals('z_custom', $modules[0]->name);
-        $this->assertEquals(CMSSCANNER_MOCKFILES_PATH . '/contao/contao2/system/modules/z_custom', $modules[0]->path);
-    }
+		$this->assertCount(1, $modules);
+		$this->assertInstanceOf('Cmsgarden\Cmsscanner\Detector\Module', $modules[0]);
+		$this->assertEquals('z_custom', $modules[0]->name);
+		$this->assertEquals(CMSSCANNER_MOCKFILES_PATH . '/contao/contao2/system/modules/z_custom', $modules[0]->path);
+	}
 
 	/**
 	 * Tests that Contao 3 modules are correctly detected
-	*/
+	 */
 	public function testContao3Modules()
 	{
 		$expected = array(
 			array(
-				'name'    => 'z_custom',
-				'path'    => '/contao/contao3/system/modules/z_custom',
+				'name' => 'z_custom',
+				'path' => '/contao/contao3/system/modules/z_custom',
 				'version' => '',
 			),
 			array(
-				'name'    => 'folderpage',
-				'path'    => '/contao/contao3/system/modules/folderpage',
+				'name' => 'folderpage',
+				'path' => '/contao/contao3/system/modules/folderpage',
 				'version' => '1.2.4',
 			),
 			array(
-				'name'    => 'dcawizard',
-				'path'    => '/contao/contao3/system/modules/dcawizard',
+				'name' => 'dcawizard',
+				'path' => '/contao/contao3/system/modules/dcawizard',
 				'version' => '1.0.0',
 			),
 			array(
-				'name'    => 'terminal42/notification_center',
-				'path'    => '/contao/contao3/composer/vendor/terminal42/notification_center',
+				'name' => 'terminal42/notification_center',
+				'path' => '/contao/contao3/composer/vendor/terminal42/notification_center',
 				'version' => '1.3.2',
 			),
 		);
 
 		$actual = $this->object->detectModules(
-			new \SplFileInfo(CMSSCANNER_MOCKFILES_PATH . '/contao/contao3')
+				new \SplFileInfo(CMSSCANNER_MOCKFILES_PATH . '/contao/contao3')
 		);
 
 		$this->assertCount(count($expected), $actual);
 
-		foreach ($actual as $i => $module) {
+		foreach ($actual as $i => $module)
+		{
 			$this->assertInstanceOf('Cmsgarden\Cmsscanner\Detector\Module', $module);
 			$this->assertEquals($expected[$i]['name'], $module->name);
 			$this->assertEquals(CMSSCANNER_MOCKFILES_PATH . $expected[$i]['path'], $module->path);
