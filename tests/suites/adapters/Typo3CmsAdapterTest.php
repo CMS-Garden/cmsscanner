cd99d9b276d23ff2f7d7f8ee7a50c430ab610b4e
astridx <info@astrid-guenther.de>
2017-03-27 10:20:33 +0200

    mock data

--- tests/suites/adapters/Typo3CmsAdapterTest.php
+++ tests/suites/adapters/Typo3CmsAdapterTest.php
@@ -67,4 +67,36 @@ class Typo3CmsAdapterTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('4.5.30', $results);
         $this->assertInstanceOf('Cmsgarden\Cmsscanner\Detector\System', current($results));
     }
+	public function testModulesAreDetected()
+    {
+        $paths = array(
+            "/typo3cms/typo3_4-5",
+            "/typo3cms/typo3_6-2",
+            "/typo3cms/typo3_broken"
+        );
+
+        foreach ($paths as $path) {
+            $path = new \SplFileInfo(CMSSCANNER_MOCKFILES_PATH . $path);
+
+            print_r( $path);
+			$modules = $this->subject->detectModules($path);
+			print_r( $modules);
+
+
+         /*   $this->assertCount(4, $modules);
+            $this->assertEquals('UnitTests', $modules[0]->name);
+            $this->assertEquals('module', $modules[0]->type);
+            $this->assertEquals('1.0.0', $modules[0]->version);
+            $this->assertEquals('Unittest', $modules[1]->name);
+            $this->assertEquals('component', $modules[1]->type);
+            $this->assertEquals('1.0.0', $modules[1]->version);
+            $this->assertEquals('Content - Unittest', $modules[2]->name);
+            $this->assertEquals('plugin', $modules[2]->type);
+            $this->assertEquals('1.0.0', $modules[2]->version);
+            $this->assertEquals('unitplate', $modules[3]->name);
+            $this->assertEquals('template', $modules[3]->type);
+            $this->assertEquals('1.0.0', $modules[3]->version);*/
+        }
+    }
+
 }
