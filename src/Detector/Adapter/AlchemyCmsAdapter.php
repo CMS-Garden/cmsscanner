da44a3dfb4bebf1bfe978d23efffab1be13590eb
zero-24 <zero-24@users.noreply.github.com>
2017-10-10 00:27:35 +0200

    update copy to 2014 - 2017

--- src/Detector/Adapter/AlchemyCmsAdapter.php
+++ src/Detector/Adapter/AlchemyCmsAdapter.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    CMSScanner
- * @copyright  Copyright (C) 2014 CMS-Garden.org
+ * @copyright  Copyright (C) 2014 - 2017 CMS-Garden.org
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link       http://www.cms-garden.org
  */
